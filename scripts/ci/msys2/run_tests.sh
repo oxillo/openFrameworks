2aa9f3f74420f313e35093fd5c3a59808cf9993e
Olivier XILLO <oxillo@hotmail.com>
2016-05-10 06:21:12 +0200

    adapt run_tests for 64 bits

---
+++ scripts/ci/msys2/run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ev
+ROOT=$(pwd -P)
+
+echo "**** Running unit tests ****"
+cd $ROOT/tests
+for group in *; do
+	if [ -d $group ]; then
+		for test in $group/*; do
+			if [ -d $test ]; then
+				cd $test
+				cp ../../../scripts/templates/msys2/Makefile .
+				cp ../../../scripts/templates/msys2/config.make .
+				make Debug
+				cd bin
+				binname=$(basename ${test})
+				if [ "${MSYSTEM:0:7}" == "MINGW64" ] ; then 
+					binname=${binname}_x64
+				fi
+                #gdb -batch -ex "run" -ex "bt" -ex "q \$_exitcode" ./${binname}_debug
+				./${binname}_debug.exe
+				errorcode=$?
+				if [[ $errorcode -ne 0 ]]; then
+					exit $errorcode
+				fi
+				cd $ROOT/tests
+			fi
+		done
+	fi
+done
