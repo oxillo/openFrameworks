aed6d7e42e0e4c0851f905ff6572ba1fdc44490f
Olivier XILLO <oxillo@hotmail.com>
2022-05-31 23:00:47 +0200

    debug

--- scripts/ci/msys2/build.sh
+++ scripts/ci/msys2/build.sh
@@ -9,7 +9,7 @@ make ${USE_CCACHE} MAKEFILE_DEBUG=true -j4 Debug
 
 echo "**** Building emptyExample ****"
 cd $ROOT/scripts/templates/msys2
-env |grep MINGW
+env | grep MINGW
 make ${USE_CCACHE} -j4 Debug
 
 echo "**** Building allAddonsExample ****"
