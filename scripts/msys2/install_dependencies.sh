13ba5f237500cba83dfbe16b0c69e13016be39fc
Olivier XILLO <oxillo@hotmail.com>
2023-03-18 19:43:51 +0100

    debug

--- scripts/msys2/install_dependencies.sh
+++ scripts/msys2/install_dependencies.sh
@@ -48,6 +48,7 @@ for pkg in ${mingwPackages}; do
 	packages="$packages  $MINGW_PACKAGE_PREFIX-$pkg"
 done
 
+echo packages = ${packages}
 # Install packages
 if [[ "${confirm}" == "yes" ]]; then
 	for pkg in ${packages}; do
