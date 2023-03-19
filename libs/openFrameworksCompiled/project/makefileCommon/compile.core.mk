eefafb8df30f1479bf9612347b047f6898c207ef
Olivier XILLO <oxillo@hotmail.com>
2023-03-19 12:36:13 +0100

    where AR

--- libs/openFrameworksCompiled/project/makefileCommon/compile.core.mk
+++ libs/openFrameworksCompiled/project/makefileCommon/compile.core.mk
@@ -266,6 +266,10 @@ else
 $(TARGET) : $(OF_CORE_OBJ_FILES) $(OF_CORE_OBJ_OUTPUT_PATH).compiler_flags
 	@echo "Creating library " $(TARGET)
 	@mkdir -p $(@D)
+	@echo PLATFORM_AR=$(PLATFORM_AR)
+	@echo PROJECT_AR=$(PROJECT_AR)
+	@echo AR=$(AR)
+	@echo PWD=$(PWD)
 	@echo $(AR) ${ARFLAGS} "$@" $(OF_CORE_OBJ_FILES)
 	$(AR) ${ARFLAGS} "$@" $(OF_CORE_OBJ_FILES)
 endif
