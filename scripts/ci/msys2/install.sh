07429b2888260c24eb986c05fe5cab33dcb3dabb
Olivier XILLO <oxillo@hotmail.com>
2023-03-19 10:16:41 +0100

    mingw32-make

--- scripts/ci/msys2/install.sh
+++ scripts/ci/msys2/install.sh
@@ -2,5 +2,5 @@
 set -ev #verbose; exit immediatly
 ROOT=$(pwd -P)
 
-$ROOT/scripts/msys2/install_dependencies.sh --noconfirm
+#$ROOT/scripts/msys2/install_dependencies.sh --noconfirm
 $ROOT/scripts/msys2/download_libs.sh --silent
